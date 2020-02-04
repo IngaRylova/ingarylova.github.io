8f0d5f5a331bebba07abf49545adef1fe5ba8072
IngaRylova <52601913+IngaRylova@users.noreply.github.com>
2020-02-04 22:33:58 +0200

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,22 @@
+ $(document).ready(function(){
+//   $('.your-class').slick({
+//     setting-name: setting-value
+//   });
+// });
+
+ 
+ $('.slider1').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
+});
